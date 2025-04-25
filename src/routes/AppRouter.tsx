e4ae48cb752444fa95e2dd97923532657bea1399
sepp <sepp.hashemi.services@gmail.com>
2025-04-25 21:48:19 +0200

    appRouter added

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,26 @@
+// src/routes/AppRouter.tsx
+import React, { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from '../components/Layout'
+import NotFound from '../pages/NotFound'
 
-const AppRouter = () => {
-  return (
-    <div>AppRouter</div>
-  )
-}
+const Home = lazy(() => import('../pages/Home'))
+const Products = lazy(() => import('../pages/Products'))
+const ProductDetail = lazy(() => import('../pages/ProductDetail'))
+const About = lazy(() => import('../pages/About'))
+
+const AppRouter: React.FC = () => (
+  <Suspense fallback={<div>Loading…</div>}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetail />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </Suspense>
+)
 
 export default AppRouter
