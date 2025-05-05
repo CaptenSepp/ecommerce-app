b09d87606c732f93a03d116f41910985a39fd300
sepp <sepp.hashemi.services@gmail.com>
2025-05-05 22:41:31 +0200

    feat (provider): implement query client management with getQueryClient function

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { getQueryClient } from './store/queryClient';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import { store } from './store/store';
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={getQueryClient()}>
         <App />
       </QueryClientProvider>
     </Provider>
