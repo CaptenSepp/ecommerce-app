a745c1c7c69a5dee7c85e7e1e234db20b5557db2
sepp <sepp.hashemi.services@gmail.com>
2025-05-10 14:31:12 +0200

    feat: implement new layout structure; integrate Header, Main, and Footer components

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,24 +1,56 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import Layout from '../layouts/Layout';
-import About from '../pages/About';
-import Cart from '../pages/Cart';
-import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
-import ProductDetails from '../pages/ProductDetails';
-import Products from '../pages/Products';
-import Wishlist from '../pages/Wishlist';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Layout from "../pages/layout/Layout";
+import About from "../pages/About";
+import Cart from "../pages/Cart";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import ProductDetails from "../pages/ProductDetails";
+import Products from "../pages/Products";
+import Wishlist from "../pages/Wishlist";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="products" element={<Products />} />
-      <Route path="products/:productId" element={<ProductDetails />} />
-      <Route path="cart" element={<Cart />} />
-      <Route path="wishlist" element={<Wishlist />} />
-      <Route path="about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-
+    <Route
+      path="/"
+      element={<Layout />}
+    >
+      <Route
+        index
+        element={<Home />}
+      />
+      <Route
+        path="products"
+        element={<Products />}
+      />
+      <Route
+        path="products/:productId"
+        element={<ProductDetails />}
+      />
+      <Route
+        path="cart"
+        element={<Cart />}
+      />
+      <Route
+        path="wishlist"
+        element={<Wishlist />}
+      />
+      <Route
+        path="about"
+        element={<About />}
+      />
+      <Route
+        path="*"
+        element={<NotFound />}
+      />
+      <Route
+        path="login"
+        element={<Login />}
+      />
     </Route>
   )
 );
